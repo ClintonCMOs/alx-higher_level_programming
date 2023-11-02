e68af3a52cdf6513bb1ed1852a6a231a9732117c
ClintonCMOs <orengecmo@gmail.com>
2023-11-03 01:31:13 +0300

    updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
     args = len(sys.argv)
     if(args <= 1):
         print("{} arguments.".format(args - 1))
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
-    for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]))
+        for x in range(1, args):
+            print("{}: {}".format(x, sys.argv[x]))
