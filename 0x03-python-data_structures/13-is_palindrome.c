a6082ac31a1a75123c630634707ba634d1fdabf9
ClintonCMOs <orengecmo@gmail.com>
2023-11-07 02:11:34 +0300

    updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,79 +1,68 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
+#include <stddef.h>
 /**
- * is_palindrome - checks for list is palindrome
- * @head: head of linked list
- * Return:  boolean
+ * reverse_listint - reverses a singly linked list
+ * @head: ptr to the ptr to head of the linked list
+ * Return: A pointer to the head of the reversed singly linkec list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *_node = *head;
+	listint_t *nxtnode;
+	listint_t *prevnode = NULL;
+
+	while (_node)
+	{
+		nxtnode = _node->next;
+		_node->next = prevnode;
+		prevnode = _node;
+		_node = nxtnode;
+	}
+	*head = prevnode;
+	return (*head);
+}
+/**
+ * is_palindrome - checks if the singly list is a palindrome
+ * @head: Pointer to pointer to head of the singly linked list
+ * Return: 0 if not palindrome, 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	int len = 0, i = 0;
-	listint_t *tmp;
-	int Ns[10000];
+	listint_t *tempo, *_rev, *middle;
+	size_t size = 0;
+	size_t v = 0;
 
-	tmp = *head;
-	if ((*head) == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (tmp != NULL)
+	tempo = *head;
+	while (tempo)
 	{
-		len++;
-		tmp = tmp->next;
+		size++;
+		tempo = tempo->next;
 	}
-	if (len == 1)
-		return (1);
-	tmp = *head;
-	while (tmp != NULL)
-	{
-		Ns[i] = tmp->n;
-		tmp = tmp->next;
-		i++;
-	}
-	for (i = 0; i <= len / 2; i++)
-	{
-		if (Ns[i] != Ns[len - i - 1])
-		{
-			return (0);
-		}
-	}
-	return (1);
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
-/**
- * is_palindrome - checks for list is palindrome
- * @head: head of linked list
- * Return:  boolean
- */
-int is_palindrome(listint_t **head)
-{
-	int len = 0, i = 0;
-	listint_t *tmp;
-	int Ns[10000];
+	tempo = *head;
 
-	tmp = *head;
-	if ((*head) == NULL)
-		return (1);
-	while (tmp != NULL)
+	while (v < (size / 2) - 1)
 	{
-		len++;
-		tmp = tmp->next;
+		tempo = tempo->next;
+		v++;
 	}
-	if (len == 1)
-		return (1);
-	tmp = *head;
-	while (tmp != NULL)
+	if ((size % 2) == 0 && tempo->n != tempo->next->n)
+		return (0);
+	tempo = tempo->next->next;
+	_rev = reverse_listint(&tempo);
+	middle = _rev;
+
+	tempo = *head;
+	while (_rev)
 	{
-		Ns[i] = tmp->n;
-		tmp = tmp->next;
-		i++;
-	}
-	for (i = 0; i <= len / 2; i++)
-	{
-		if (Ns[i] != Ns[len - i - 1])
-		{
+		if (tempo->n != _rev->n)
 			return (0);
-		}
+		tempo = tempo->next;
+		_rev = _rev->next;
 	}
+	reverse_listint(&middle);
 	return (1);
-}}
+}
