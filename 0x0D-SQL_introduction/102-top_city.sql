517d9d5d35426f46a94c601d01629070d9fc9bf9
ClintonCMOs <orengecmo@gmail.com>
2024-01-16 18:51:01 -0800

    updated 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,5 @@
--- display average temp(farenheit) by city ordered by temp (descending)
-SELECT `city`, AVG(`value`) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
+-- average by city
+SELECT city, AVG(value) AS avg_temp FROM
+(SELECT city, month, value FROM temperatures
+WHERE month=7 OR month=8) AS A
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
