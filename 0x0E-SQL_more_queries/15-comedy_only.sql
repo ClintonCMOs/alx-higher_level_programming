2a29aa4177e7cba9b7d49d8fa7f11925bec95939
ClintonCMOs <orengecmo@gmail.com>
2024-01-17 15:03:59 -0800

    updated  15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,9 @@
 -- only comedy
-SELECT ts.title FROM tv_shows AS ts
-JOIN tv_show_genres AS tgs
-ON ts.id = tgs.show_id
-JOIN tv_genres AS tgg
-ON tgs.genre_id = tgg.id
-WHERE tgg.name = 'Comedy'
-GROUP BY ts.title ASC;
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
