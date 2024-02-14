717f550aaf0b38c08ad32879f1e4908831094133
ClintonCMOs <orengecmo@gmail.com>
2024-02-14 00:27:42 -0800

    2-arguments.js updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ const ArgCount = process.argv.length;
 if (ArgCount === 2) {
 	console.log('No argument');
 } else if (ArgCount === 3) {
-	console.log('Argument found')
+	console.log('Argument found');
 } else {
-	console.log('Arguments found')
+	console.log('Arguments found');
 }
