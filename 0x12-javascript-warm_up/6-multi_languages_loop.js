bec8f0f17718fb6da7d98be9ba1f86980daebc06
ClintonCMOs <orengecmo@gmail.com>
2024-02-14 16:35:19 +0300

    updated 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const my_Array = ['C if fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < my_Array.length; i++) {
-  console.log(my_Array[i]);
-}
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+        console.log(line);
+      }
