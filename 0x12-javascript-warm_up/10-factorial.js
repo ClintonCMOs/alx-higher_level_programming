b62e02aecfeb3bde199601c7953f92f490f48c06
ClintonCMOs <orengecmo@gmail.com>
2024-02-14 06:33:18 -0800

    updated 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (isNaN(n) || n <= 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+function factorial (n) {
+  if (n === 1) {
+    return 1;
+  } else if (n > 1) {
+    return n * (factorial(n - 1));
+  }
 }
 
-const number = parseInt(process.argv[2]);
-
-console.log(factorial(number));
+const myNumber = parseInt(process.argv[2]);
+if (isNaN(process.argv[2])) {
+  console.log(1);
+} else {
+  console.log(factorial(myNumber));
+}
