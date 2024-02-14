4d26eab80d58d05cecb30763f5fd6abfabfcf59a
ClintonCMOs <orengecmo@gmail.com>
2024-02-14 00:30:10 -0800

    2-arguments.js updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
 
 const ArgCount = process.argv.length;
-
-if (ArgCount === 2) {
-	console.log('No argument');
-} else if (ArgCount === 3) {
-	console.log('Argument found');
-} else {
-	console.log('Arguments found');
-}
+console.log(ArgCount === 2 ? 'No argument' : ArgCount === 3 ? 'Argument found' : 'Arguments found');
