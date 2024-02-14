c91cd99c3a84f49ee7d655541ed7bf3d4d449fff
ClintonCMOs <orengecmo@gmail.com>
2024-02-14 01:26:23 -0800

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
 
-const Arg_length = process.argv[2];
-
-console.log(Arg_length === undefined ? 'No argument' : Arg_length);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
