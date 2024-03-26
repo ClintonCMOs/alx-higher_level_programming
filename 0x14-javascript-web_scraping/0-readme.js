eda41080030c1ee496a50024ee6ba27958e927db
ClintonCMOs <orengecmo@gmail.com>
2024-03-26 23:23:45 +0300

    A script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const fileName = process.argv[2];
-
-fs.readFile(fileName, 'utf-8', (error, content) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
