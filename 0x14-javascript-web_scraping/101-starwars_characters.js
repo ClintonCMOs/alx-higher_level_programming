eca43d7c7a5eec1b7f925ceb7b981303f80701e2
ClintonCMOs <orengecmo@gmail.com>
2024-03-27 00:11:16 +0300

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,30 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
+const request = require('request'); // Use const for requiring modules
+
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`; // Use template literals for string concatenation
+
+// Use arrow functions for callbacks
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) { // Check if response is successful
     const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
+  } else {
+    console.error(error); // Proper error handling
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
+// Use arrow functions for callbacks
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return; // Exit condition for recursion
+  }
+
+  request(characters[index], (error, response, body) => {
+    if (!error && response.statusCode === 200) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+      printCharacters(characters, index + 1);
+    } else {
+      console.error(error); // Proper error handling
     }
   });
 }
